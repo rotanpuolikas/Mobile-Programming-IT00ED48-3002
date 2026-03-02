de8b5a6d8650631e46aa496ec6437be3e62225ab
rotanpuolikas <tuukka.alatalo@gmail.com>
2026-03-02 14:24:42 +0200

    fixed a mysterious firebase auth bug that caused console errors

--- firebase.js
+++ firebase.js
@@ -1,11 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage"
 
 import firebaseConfig from './firebase-config.js' // contains secrets :)
 
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// export const auth = getAuth(app); shows a warning but doesn't really break anything...
+export const auth = initializeAuth(app, {persistence: getReactNativePersistence(ReactNativeAsyncStorage)})
 export const db = getFirestore(app);
--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,5 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
+import { useContext } from "react"
+
+import { TouchableOpacity, Text } from "react-native"
+
+import { AuthContext } from "../context/AuthContext"
+
 import LocationListScreen from "../screens/LocationListScreen"
 import AddLocationScreen from "../screens/AddLocationScreen"
 import MapScreen from "../screens/MapScreen"
@@ -10,8 +16,18 @@ const Tab = createBottomTabNavigator()
 // basic bottom navigation bar thingy
 
 const MainNavigator = () => {
+
+  const { logout, user } = useContext(AuthContext)
+  
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerRight: () => (
+          <TouchableOpacity onPress={logout} style={{ marginRight: 15 }}>
+            <Text style={{ color: "#E63946", fontWeight: "600" }}>
+              Logout
+            </Text>
+          </TouchableOpacity>), headerTitle: `Welcome ${user?.email}`,}}>
       <Tab.Screen name="Locations" component={LocationListScreen} />
       <Tab.Screen name="Add Location" component={AddLocationScreen} />
       <Tab.Screen name="Map" component={MapScreen} />
